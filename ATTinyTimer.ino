1ee56c9308f1b5e58ff722f636ab9cbe81f9ae8d
chrisco210 <chrisrachlinski@gmail.com>
2018-08-17 10:05:36 -0400

    mostly working

--- ATTinyTimer.ino
+++ ATTinyTimer.ino
@@ -1,43 +1,71 @@
 //How long you want the interval to be, positive integer in seconds
-#define INTERVAL 60
+#define INTERVAL 150
 
 #define PIN_VOUT 0
-#define PIN_DONE 1
+#define PIN_DONE 3
+#define PIN_DBG 4
+#define PIN_FUNC 1
 
 
-const unsigned long long interval = INTERVAL;
-unsigned long long last;
+const long interval = INTERVAL * 1000000;
+const long blinkInterval = 1000000 / 2;
+
+unsigned long last;
+  
+unsigned long ledLast;
 bool currentState;
+bool dbgLed = false;
 
 void setup() {
+  pinMode(PIN_FUNC, OUTPUT);
   pinMode(PIN_VOUT, OUTPUT);
-  pinMode(PIN_DONE, INPUT);
+  pinMode(PIN_DBG, OUTPUT);
+    digitalWrite(PIN_FUNC, LOW);
 
-  
+
   setState(true);
-  last = millis();
+  ledLast = micros();
+  last = micros();
 }
 
 void loop() {
-  if((millis() - last) > (interval * 1000)) {
+  unsigned long micro = micros();
+  
+  if((micro - last) >= (interval)) {
     cycle();
-  } else if(digitalRead(PIN_DONE) == HIGH) {
+    
+  } else if(analogRead(PIN_DONE) * (5.0 / 1023.0) > 2.9) {
     setState(false);
-  } else {
+  } 
+
+  if(micro - ledLast > blinkInterval) {
+    digitalWrite(PIN_DBG, dbgLed);
+    dbgLed = !dbgLed;
+    ledLast = micros();
   }
+
+  //free(&micro);
   
 }
 
 void cycle() {
   setState(false);
-  delay(500);
+  delay(5);
   setState(true);
-  last = millis();
+  last = micros();
 }
 
 //True for on, false for off
 void setState(bool state) {
-  currentState = state;
-  digitalWrite(PIN_VOUT, !state);
+  digitalWrite(PIN_FUNC, HIGH);
+  if(state) {
+    digitalWrite(PIN_VOUT, LOW);
+  } else {
+    digitalWrite(PIN_VOUT, HIGH);
+  }
+
+  //free(&state);
+  digitalWrite(PIN_FUNC, LOW);
 }
+
 
